4a6132a5bccf3780ed61aeb6fa1e0586d9f817cc
Zelo101 <umadeguy@gmail.com>
2024-07-10 12:50:03 +0100

    Integrate wiki.ts into route

--- src/routes/wiki/+page.svelte
+++ src/routes/wiki/+page.svelte
@@ -1,9 +1,43 @@
 <script lang="ts">
-    import { groupSections, parseRaw } from "$lib/wiki";
+    // @ts-ignore
     import indexRaw from "$lib/index.svelte.ts?raw";
-    import RuleComponent from "$lib/components/RuleComponent.svelte";
-    import { parseRules } from "$lib/parse";
     import ExampleRuleComponent from "$lib/components/ExampleRuleComponent.svelte";
+
+    type Section = Partial<Record<string, string>>
+    type SectionGroup = Record<string, Section[]>
+
+    const parseRaw = (raw: string) => {
+        const sections: Section[] = [];
+        let currentSection: Section = {}
+
+        const lines = raw.split("\n");
+        for (const line of lines) {
+            const jsDoc = line.match(/^ \* @(\w+)\s(.+)/);
+            if (line === "/**") currentSection = {}
+            if (jsDoc) {
+                const [, key, value] = jsDoc;
+                currentSection[key] = value;
+            }
+            if (line === " */") sections.push(currentSection)
+        }
+
+        return sections;
+    }
+
+    const groupSections = (sections: Section[]) => {
+        const groups: SectionGroup = {};
+        for (const section of sections) {
+            let group = section.group;
+            if (!group) {
+                group = "Ungrouped"
+            }
+            // Create array if this is a new group name
+            if (!groups[group]) groups[group] = [];
+            groups[group].push(section);
+        }
+
+        return groups;
+    }
 
     const sections = parseRaw(indexRaw)
     const groups = Object.entries(groupSections(sections))
